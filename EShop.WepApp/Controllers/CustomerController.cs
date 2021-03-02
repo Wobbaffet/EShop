ea3319e21e9861a35d8652b1d2add80820261f4b
Wobafet <53840207+Wobafet@users.noreply.github.com>
2021-03-02 16:02:24 +0100

    Dodata stranica SignUp i controller Customer

---
+++ EShop.WepApp/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using EShop.Data.UnitOfWork;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.WepApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private IUnitOfWork uow;
+        public CustomerController(IUnitOfWork uow)
+        {
+            this.uow = uow;
+        }
+
+        public ActionResult SignUp()
+        {
+            return View("SignUp");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
