b48721640876bb3680f19e8c4df1308547c7141a
AleksaGK <aleksamileticgk@gmail.com>
2021-03-07 19:42:27 +0100

    added BookId in OredItem

--- EShop/Migrations/20210307182059_addedBook.Designer.cs
+++ EShop/Migrations/20210307182059_addedBook.Designer.cs
@@ -4,14 +4,16 @@ using EShop.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EShop.Model.Migrations
 {
     [DbContext(typeof(ShopContext))]
-    partial class ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20210307182059_addedBook")]
+    partial class addedBook
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EShop/Migrations/20210307182059_addedBook.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EShop.Model.Migrations
+{
+    public partial class addedBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BookId",
+                table: "OrderItem",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_BookId",
+                table: "OrderItem",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Book_BookId",
+                table: "OrderItem",
+                column: "BookId",
+                principalTable: "Book",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Book_BookId",
+                table: "OrderItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItem_BookId",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "OrderItem");
+        }
+    }
+}
