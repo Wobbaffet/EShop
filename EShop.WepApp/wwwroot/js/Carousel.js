6f582f8248a232633970cf5d20e1e87f15f1bea5
AleksaGK <aleksamileticgk@gmail.com>
2021-03-11 20:36:49 +0100

    Added some lybraries for home page

---
+++ EShop.WepApp/wwwroot/js/Carousel.js
@@ -0,0 +1,30 @@
+﻿$('.slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
