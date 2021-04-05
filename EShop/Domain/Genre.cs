47386bfcf0be4bfbff908a0cd02fd7e22d32ed24
Wobafet <markobabovic406@gmail.com>
2021-04-05 21:37:04 +0200

    Dokumentovana klasa Genre

--- EShop/Domain/Genre.cs
+++ EShop/Domain/Genre.cs
@@ -4,12 +4,27 @@ using System.Text;
 
 namespace EShop.Model.Domain
 {
+
+    /// <summary>
+    /// Represent Genre class
+    /// </summary>
     public class Genre
     {
+        /// <value>Represent Genre id as int</value>
         public int GenreId { get; set; }
+        /// <value>Represent name as string</value>
+
         public string Name { get; set; }
+        /// <value>Represent all book genres
+        /// <para>Between genre and book is association class  BookGenre</para>
+        /// </value>
+
         public List<Book> Books { get; set; }
 
+        /// <summary>
+        /// Override string method
+        /// </summary>
+        /// <returns>string in format <c>Name</c></returns>
         public override string ToString()
         {
             return Name;
