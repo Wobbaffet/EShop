8ef5deb1ac0ec02c209f6386d3af648c40156923
Wobafet <markobabovic406@gmail.com>
2021-04-05 21:49:34 +0200

    Dokumentovana klasa Order

--- EShop/Domain/Order.cs
+++ EShop/Domain/Order.cs
@@ -4,14 +4,26 @@ using System.Text;
 
 namespace EShop.Model.Domain
 {
+    /// <summary>
+    /// Represent Order class
+   
+    /// </summary>
     public class Order
     {
+        /// <value>Represent order id as int</value>
         public int OrderId { get; set; }
-        public DateTime Date { get; set; }
-        public double Total { get; set; }
-        public OrderStatus OrderStatus { get; set; }
-        public List<OrderItem> OrderItems { get; set; }
+        /// <value>Represent purchased date as DateTime class</value>
 
+        public DateTime Date { get; set; }
+        /// <value>Represent sum of all book price in order
+
+        public double Total { get; set; }
+        /// <value>Represent order status as enum OrderStatus
+        public OrderStatus OrderStatus { get; set; }
+
+        /// <value>Represent list of <c>OrderItem</c>
+        public List<OrderItem> OrderItems { get; set; }
+        /// <value>Represent reference to <c>Customer</c></value>
         public Customer Customer { get; set; }
         public int CustomerId { get; set; }
     }
