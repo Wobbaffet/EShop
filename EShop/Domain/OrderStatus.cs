11779a87bf358d9b75177dd7e019fadc35894fce
Wobafet <markobabovic406@gmail.com>
2021-04-05 21:43:13 +0200

    Dokumentovan enum OrderStatus

--- EShop/Domain/OrderStatus.cs
+++ EShop/Domain/OrderStatus.cs
@@ -4,6 +4,29 @@ using System.Text;
 
 namespace EShop.Model.Domain
 {
+    /// <summary>
+    /// Represent enum for Order status
+    /// <remarks>
+    /// Contains status:
+    /// 
+    /// <list type="bullet">
+    /// <item>
+    /// <term>Uncompleted</term>
+    /// </item>
+    ///  <item>
+    /// <term>InProgress</term>
+    /// </item>
+    ///  <item>
+    /// <term>Shipped</term>
+    /// </item>  
+    /// <item>
+    /// <term>Completed</term>
+    /// </item>
+    /// 
+    /// </list>
+    /// 
+    /// </remarks>
+    /// </summary>
     public enum OrderStatus
     {
         Uncompleted,
