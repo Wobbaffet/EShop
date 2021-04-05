5585003dc6882be02b76fc0f7edcd58c999f7908
Wobafet <markobabovic406@gmail.com>
2021-04-05 21:40:30 +0200

    Dokumentovana klasa Address

--- EShop/Domain/Address.cs
+++ EShop/Domain/Address.cs
@@ -4,13 +4,25 @@ using System.Text;
 
 namespace EShop.Model.Domain
 {
+    /// <summary>
+    /// Represent Address class
+    /// <remarks>
+    /// Address class is merged in database with Customer class
+    /// <para>This is 1 to 1 relation</para>
+    /// </remarks>
+    /// </summary>
     public class Address
     {
-        //public int AddressId { get; set; }
+        /// <value>Represent street name  as string</value>
         public string StreetName { get; set; }
+        /// <value>Represent street number  as string</value>
+
         public string StreetNumber { get; set; }
+        /// <value>Represent city name as string</value>
+
         public string CityName { get; set; }
+        /// <value>Represent ptt as int</value>
+
         public int PTT { get; set; }
-        /*public Customer Customer { get; set; }*/
     }
 }
