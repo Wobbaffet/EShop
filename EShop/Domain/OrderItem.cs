2ac0d774372a8fc6e44e477142d0f326531d223d
Wobafet <markobabovic406@gmail.com>
2021-04-05 21:54:03 +0200

    Dokumentovana Order item klasa

--- EShop/Domain/OrderItem.cs
+++ EShop/Domain/OrderItem.cs
@@ -5,11 +5,16 @@ using System.Text;
 
 namespace EShop.Model.Domain
 {
+    /// <summary>
+    /// Represent OrderItem class
+    /// </summary>
     public class OrderItem
     {
+        /// <value>Represent order item id as int</value>
         public int OrderItemId { get; set; }
+        /// <value>Represent quantity of book</value>
         public int Quantity { get; set; }
-       
+        /// <value>Reference to Book class</value>
         public Book Book { get; set; }
       
 
